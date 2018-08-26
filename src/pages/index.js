aee3da4f896ff4e04bff2d996c1935de9594ab40
Mike Lothar <mikelothar@gmail.com>
2018-08-26 13:52:08 +0200

    add both hands option

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,38 +1,62 @@
 import React from 'react'
 
-const fixedNotes = ['C major', 'G major', 'D major', 'A major', 'E major', 'B major', 'F major']
-let notes = [...fixedNotes]
+const allOneHandNotes = ['C major', 'G major', 'D major', 'A major', 'E major', 'B major', 'F major']
+const allBothHandNotes = ['C major', 'G major']
 
 class IndexPage extends React.Component {
   constructor() {
     super()
 
-    this.state = {
-      note: 'Begin',
-      rightHand: true,
-    }
+    this.scaleIndex = 0
+    this.scales = []
 
     this.getNote = this.getNote.bind(this)
+    this.reloadScales = this.reloadScales.bind(this)
+
+    this.reloadScales()
+
+    this.state = {
+      note: 'Begin',
+    }
+  }
+
+  reloadScales() {
+    this.scales = [
+      {
+        hand: 'Right hand',
+        notes: [...allOneHandNotes],
+      },
+      {
+        hand: 'Left hand',
+        notes: [...allOneHandNotes],
+      },
+      {
+        hand: 'Both hands',
+        notes: [...allBothHandNotes],
+      },
+    ]
   }
 
   getNote() {
-    if (notes.length === 0) {
-      notes = [...fixedNotes]
-      this.setState(prevState => {
-        return {
-          rightHand: !prevState.rightHand,
-        }
-      })
-    }
-
-    const noteIndex = Math.floor(Math.random() * notes.length)
-    const note = notes[noteIndex]
+    const noteIndex = Math.floor(Math.random() * this.scales[this.scaleIndex].notes.length)
+    const note = this.scales[this.scaleIndex].notes[noteIndex]
 
     if (note !== this.state.note) {
-      this.setState({ note: note })
-      notes.splice(noteIndex, 1)
+      this.setState({
+        note: note,
+        hand: this.scales[this.scaleIndex].hand,
+      })
+      this.scales[this.scaleIndex].notes.splice(noteIndex, 1)
     } else {
       this.getNote()
+    }
+
+    if (this.scales[this.scaleIndex].notes.length === 0) {
+      this.scaleIndex++
+      if (this.scaleIndex >= this.scales.length) {
+        this.scaleIndex = 0
+        this.reloadScales()
+      }
     }
   }
 
@@ -41,7 +65,7 @@ class IndexPage extends React.Component {
       <div className="note-container" onClick={this.getNote}>
         <div className="note">{this.state.note}</div>
         <div className="hand">
-          {this.state.note !== 'Begin' && (this.state.rightHand ? 'Right hand' : 'Left hand')}
+          {this.state.hand}
         </div>
       </div>
     )
