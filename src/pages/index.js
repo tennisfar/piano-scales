97b93fb44b849a5afbcf1c3b2aae795f14b083ae
Mike Lothar <mikelothar@gmail.com>
2018-08-26 12:24:43 +0200

    alternate hands, repeat only scales one time in each hand

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,48 @@
 import React from 'react'
 
+const fixedNotes = ['C major', 'G major', 'D major', 'A major', 'E major', 'B major', 'F major']
+let notes = [...fixedNotes]
+
 class IndexPage extends React.Component {
   constructor() {
     super()
-    this.state = { note: 'Click' }
+
+    this.state = {
+      note: 'Begin',
+      rightHand: true,
+    }
+
+    this.getNote = this.getNote.bind(this)
+  }
+
+  getNote() {
+    if (notes.length === 0) {
+      notes = [...fixedNotes]
+      this.setState(prevState => {
+        return {
+          rightHand: !prevState.rightHand,
+        }
+      })
+    }
+
+    const noteIndex = Math.floor(Math.random() * notes.length)
+    const note = notes[noteIndex]
+
+    if (note !== this.state.note) {
+      this.setState({ note: note })
+      notes.splice(noteIndex, 1)
+    } else {
+      this.getNote()
+    }
   }
 
   render() {
-    const notes = ['C major', 'G major', 'D major', 'A major', 'E major', 'B major', 'F major']
-
-    const getNote = () => {
-      const note = notes[Math.floor(Math.random() * notes.length)]
-      
-      if (note !== this.state.note) {
-        this.setState({ note: note })
-      } else {
-        getNote()
-      }
-    }
-
     return (
-      <div className="note-container"
-           onClick={getNote}>{this.state.note}
+      <div className="note-container" onClick={this.getNote}>
+        <div className="note">{this.state.note}</div>
+        <div className="hand">
+          {this.state.note !== 'Begin' && (this.state.rightHand ? 'Right hand' : 'Left hand')}
+        </div>
       </div>
     )
   }
